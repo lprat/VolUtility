b7c919026cc7849fbf25804818b2c728b0931a69
Lionel <lionel.prat9@gmail.com>
2020-12-14 16:31:49 +0100

    Update start.sh

--- VolUtility/start.sh
+++ VolUtility/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Starting MongoDB
-mongod --fork --logpath /home/volutility/mongodb.log --dbpath /home/volutility/dbpath/
+mongod --fork --logpath /opt/VolUtility/logs/mongodb.log --dbpath /opt/VolUtility/dbpath/
 sleep 5
 echo Django migrate
 python manage.py migrate
@@ -9,6 +9,6 @@ if [ ! -z "$VT_APIKEY" ]; \
         then echo "VT API Setting: $VT_APIKEY"; sed -i -e "s/api_key = None/api_key = $VT_APIKEY/g" ~/.volutility.conf; \
     fi
 sleep 5
-#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'your_email', 'your_password" | python manage.py shell
+#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'email', 'password')" | python manage.py shell
 echo Starting VolUtility
 python manage.py runserver 0.0.0.0:8080
